7d93d909d5b3e497e55d425a1125c1c3429c67f7
Roy Sukrit <64809877+roy-sukrit@users.noreply.github.com>
2021-03-05 22:30:49 +0530

    Heroku editing of API keys

--- firebase/index.js
+++ firebase/index.js
@@ -1,7 +1,9 @@
 
 var admin = require("firebase-admin");
+//**imp edit for heroku
+//var serviceAccount = require("../config/fbServiceAccountKey.json");
+var serviceAccount = JSON.parse(Buffer.from(process.env.GOOGLE_CONFIG_BASE64, 'base64').toString('ascii'))
 
-var serviceAccount = require("../config/fbServiceAccountKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
