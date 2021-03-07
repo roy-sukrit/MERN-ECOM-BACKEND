a6b6212f950a0d7e4334b2345dbdf9936091e72e
Roy Sukrit <64809877+roy-sukrit@users.noreply.github.com>
2021-03-07 13:10:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,11 +8,7 @@ require("dotenv").config();
 
 // app
 const app = express();
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+
 app.use(cors());
 
 // db
