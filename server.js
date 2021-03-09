7bd770b4d74d5581df02c4585bc147e725ca8fef
Sukrit <roysukrit78@gmail.com>
2021-03-09 16:58:27 +0530

    options

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const corsOpts = {
 };
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE','OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
